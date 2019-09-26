ef9e9ecef0dd5fe56e9b99535033e5451bd1a5cb
comical-tag-7436 <shawtynow2008@gmail.com>
2019-09-26 01:00:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var Name = "Joe"
 var height = "74"
-if (name )
+if(name === "Susan" ) {
+console.log ("The name  
+  is correct ")
+}
 // Don't worry about this
 module.exports = { name , height
 }
